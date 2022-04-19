97adf4cc302bb943e153535356109e31b8044822
王俊琦 <topi.wang@igg.com>
2022-04-19 18:20:40 +0800

    test

---
+++ test.air/test1.py
@@ -0,0 +1,21 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+
+
+#touch(Template(r"tpl1544090031428.png", record_pos=(-0.052, -0.069), resolution=(1920, 1080)))
+
+text("123")
+keyevent("67")  # 67即为删除键，请注意传入的是字符串
+
+
+
+#sleep(3)
+#touch(Template(r"tpl1544090093280.png", record_pos=(-0.065, 0.008), resolution=(1920, 1080)))
+#sleep(3)
+#text("qq123456")
+
